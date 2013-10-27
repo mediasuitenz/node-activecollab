4db8e58484ee95e653e6c2828ff4a2e80322d6f1
Richard Walker <digitalsadhu@gmail.com>
2013-10-27 17:36:01 +0100

    Add rest module for interacting with the server

---
+++ rest.js
@@ -0,0 +1,84 @@
+var request = require('superagent');
+
+/**
+ * Used to fetch (by GET) from the server.
+ * 
+ * @param  {string}   url      - url of api
+ * @param  {string}   endpoint - endpoint of api
+ * @param  {string}   apiKey   - api key
+ * @param  {Function} cb       - callback function
+ */
+module.exports.fetch = function (url, endpoint, apiKey, cb) {
+    if (typeof cb !== 'function') {
+        throw new TypeError('cb must be a function');
+    }
+    request
+        .get(url + '?path_info=' + endpoint + '&auth_api_token=' + apiKey)
+        .set('Accept', 'application/json')
+        .end(function (res) {
+            if (res.status === 200 && res.ok === true) {
+                cb(JSON.parse(res.text));
+            } else {
+                cb(false);
+            }
+        }
+    );
+};
+
+/**
+ * Used to create (by POST) items on the server.
+ * 
+ * @param  {string}   url      - url of api
+ * @param  {string}   endpoint - endpoint of api
+ * @param  {string}   apiKey   - api key
+ * @param  {object}   data     - key/value data to post
+ * @param  {Function} cb       - callback function
+ */
+module.exports.create = function (url, endpoint, apiKey, data, cb) {
+    if (typeof cb !== 'function') {
+        throw new TypeError('cb must be a function');
+    }
+    request
+        .post(url + '?path_info=' + endpoint + '&auth_api_token=' + apiKey)
+        .type('form')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .set('Accept', 'application/json')
+        .send(data)
+        .end(function (res) {
+            if (res.status === 200 && res.ok === true) {
+                cb(JSON.parse(res.text));
+            } else {
+                cb(res.error);
+            }
+        }
+    );
+};
+
+/**
+ * Used to edit (by POST) existing items on the server.
+ * 
+ * @param  {string}   url      - url of api
+ * @param  {string}   endpoint - endpoint of api
+ * @param  {string}   apiKey   - api key
+ * @param  {object}   data     - key/value data to post
+ * @param  {Function} cb       - callback function
+ */
+module.exports.edit = function (url, endpoint, apiKey, data, cb) {
+    if (typeof cb !== 'function') {
+        throw new TypeError('cb must be a function');
+    }
+    request
+        .post(url + '?path_info=' + endpoint + '&auth_api_token=' + apiKey)
+        .type('form')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .set('Accept', 'application/json')
+        .send(data)
+        .end(function (res) {
+            if (res.status === 200 && res.ok === true) {
+                cb(JSON.parse(res.text));
+            } else {
+                cb(res.error);
+            }
+        }
+    );
+};
